b9ceb23fa1ea2dfd64236dee196db4e784692fd9
bautistasaggese <bautistasaggese2002@gmail.com>
2022-07-08 10:55:50 -0300

    first commit

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
-//const parrafo = document.querySelector('#remeraMB')
-//parrafo.innerHTML = 'Remera M.B. <b>(Limitada)</b>'
+// const parrafo = document.querySelector('#remeraMB')
+// parrafo.innerHTML = 'Remera M.B. <b>(Limitada)</b>'
 
-const lista = document.getElementById('lista')
-//console.log(lista)
+// const lista = document.getElementById('lista')
+// console.log(lista)
 
 // const li = document.createElement('li')
 // li.textContent = 'primer elemento' //(insertando texto al li)
 
-//lista.appendChild(li) //agregando elemento hijo a ul(lista)
-const arrayElement = ['primer elemento', 'segundo', 'tercero']
+// lista.appendChild(li) //agregando elemento hijo a ul(lista)
+// const arrayElement = ['primer elemento', 'segundo', 'tercero']
 
 // arrayElement.forEach(item => {
 //     //console.log(item)
